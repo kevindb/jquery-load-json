55cb06189058cd058c9dfdc1fd140b3cdd363f8e
jocapc@gmail.com <jocapc@gmail.com@91f6fdaf-c773-323d-6236-93afe5a5d5da>
2011-04-25 15:44:36 +0000

    Initial version

---
+++ trunk/data17.js
@@ -0,0 +1,1 @@
+﻿{"ID":17,"Name":"Emkay Entertainments","Address":"Nobel House, Regent Centre","Town":"Lothian","Description":null,"Date":"\/Date(-62135596800000)\/","Contact":"Phone","IsFeatured":true,"Country":["UK","USA"]}
---
+++ trunk/list.js
@@ -0,0 +1,63 @@
+"data":[
+{
+	"ID": 17,
+        "Name": "Emkay Entertainments",
+        "Address": "Nobel House, Regent Centre",
+        "Town": "Lothian",
+        "Description": null,
+        "Contact": "Phone",
+        "IsFeatured": true,
+        "Country": [
+            "UK",
+            "USA" 
+        ] 
+    },
+    {
+        "ID": 18,
+        "Name": "The Empire",
+        "Address": "Milton Keynes Leisure Plaza",
+        "Town": "Buckinghamshire",
+        "Description": null,
+        "Date": "\/Date(-62135596800000)\/",
+        "Contact": "Post",
+        "IsFeatured": false,
+        "Country": null 
+    },
+    {
+        "ID": 19,
+        "Name": "Asadul Ltd",
+        "Address": "Hophouse",
+        "Town": "Essex",
+        "Description": null,
+        "Date": "\/Date(-62135596800000)\/",
+        "Contact": "Email",
+        "IsFeatured": false,
+        "Country": [
+            "UK" 
+        ] 
+    },
+    {
+        "ID": 20,
+        "Name": "Star Records",
+        "Address": null,
+        "Town": null,
+        "Description": null,
+        "Date": "\/Date(-62135596800000)\/",
+        "Contact": "Email",
+        "IsFeatured": false,
+        "Country": [
+            "SRB" 
+        ] 
+    },
+    {
+        "ID": 21,
+        "Name": "Ashley Mark Publishing Company",
+        "Address": "1-2 Vance Court",
+        "Town": "Tyne & Wear",
+        "Description": null,
+        "Date": "\/Date(-62135596800000)\/",
+        "Contact": null,
+        "IsFeatured": true,
+        "Country": null 
+    }
+]
