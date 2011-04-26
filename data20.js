bd19cfb52a4862c9a1c3ce503b531407c0dd5854
jocapc@gmail.com <jocapc@gmail.com@91f6fdaf-c773-323d-6236-93afe5a5d5da>
2011-04-26 06:08:06 +0000

    Added json object containing the data that should be loaded into the HTML

---
+++ data20.js
@@ -0,0 +1,14 @@
+﻿{
+    "ID": 20,
+    "Name": "Star Records",
+    "Address": "Nobel House, Regent Centre",
+    "Town": "London",
+    "Description": "",
+    "Date": "\/Date(-62135596800000)\/",
+    "Contact": "Phone",
+    "IsFeatured": true,
+    "Country": [
+        "SRB",
+        "UK" 
+    ]
+}
