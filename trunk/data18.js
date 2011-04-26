f012a3275a3f2f277d8ee416b0a2d3bbab26f549
jocapc@gmail.com <jocapc@gmail.com@91f6fdaf-c773-323d-6236-93afe5a5d5da>
2011-04-26 06:08:06 +0000

    Added json object containing the data that should be loaded into the HTML

---
+++ trunk/data18.js
@@ -0,0 +1,13 @@
+﻿{
+    "ID": 18,
+    "Name": "The Empire",
+    "Address": "Milton Keynes Leisure Plaza",
+    "Town": "London",
+    "Description": "Lorem ipsum...",
+    "Date": "\/Date(-62135596800000)\/",
+    "Contact": "Email",
+    "IsFeatured": false,
+    "Country": [
+        "UK" 
+    ]
+}
---
+++ trunk/data19.js
@@ -0,0 +1,11 @@
+﻿{
+    "ID": 19,
+    "Name": "Asadul Ltd",
+    "Address": "Hophouse",
+    "Town": "Essex",
+    "Description": "",
+    "Date": "\/Date(-62135596800000)\/",
+    "Contact": "Phone",
+    "IsFeatured": false,
+    "Country": null
+}
