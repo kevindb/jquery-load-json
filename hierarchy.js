e64f260fb71c917be85d0336d47e44c7386977a6
jocapc@gmail.com <jocapc@gmail.com@91f6fdaf-c773-323d-6236-93afe5a5d5da>
2011-04-29 16:27:04 +0000

    JSON data source for hierarchy example

---
+++ hierarchy.js
@@ -0,0 +1,32 @@
+﻿[
+    {   "ID": 17, "Name": "Emkay Entertainments", "Address": "Nobel House, Regent Centre",
+        "Manager": { "ID": 17, "FirstName": "Nick", "LastName": "Donovan" },
+        "Employees": [  { "ID": 18, "FirstName": "John", "LastName": "Doe" },
+                        { "ID": 19, "FirstName": "Alex", "LastName": "Watkins" },
+                        { "ID": 20, "FirstName": "Mick", "LastName": "Henning"}]
+    },
+    {   "ID": 18, "Name": "The Empire", "Address": "Milton Keynes Leisure Plaza", 
+        "Manager": { "ID": 21, "FirstName": "Ana", "LastName": "Frank" },
+        "Employees": [  { "ID": 22, "FirstName": "Mark", "LastName": "Anderson" },
+                        { "ID": 23, "FirstName": "John", "LastName": "Haus" },
+                        { "ID": 24, "FirstName": "Sinthia", "LastName": "Clouny"}]
+    },
+    {   "ID": 19, "Name": "Asadul Ltd", "Address": "Hophouse",
+        "Manager": { "ID": 25, "FirstName": "John", "LastName": "Feigan" },
+        "Employees": [  { "ID": 26, "FirstName": "Micheal", "LastName": "King" },
+                        { "ID": 27, "FirstName": "Jody", "LastName": "Palmer"}]
+    },
+    {   "ID": 20, "Name": "Star Records", "Address": "Oxford Street 22-24",
+        "Manager": { "ID": 28, "FirstName": "Steve", "LastName": "Carter" },
+        "Employees": [  { "ID": 29, "FirstName": "Andrea", "LastName": "Smith" },
+                        { "ID": 30, "FirstName": "Jane", "LastName": "Anderson" },
+                        { "ID": 31, "FirstName": "Anthony", "LastName": "Willis" },
+                        { "ID": 32, "FirstName": "George", "LastName": "Hill"}]
+    },
+    {   "ID": 21, "Name": "Ashley Mark Publishing Company", "Address": "1-2 Vance Court",
+        "Manager": { "ID": 33, "FirstName": "Micheal", "LastName": "Johnson" },
+        "Employees": [  { "ID": 34, "FirstName": "Micheal", "LastName": "Sharp" },
+                        { "ID": 35, "FirstName": "Jeff", "LastName": "Cave" },
+                        { "ID": 36, "FirstName": "Peter", "LastName": "Hayward"}]
+    }
+]
