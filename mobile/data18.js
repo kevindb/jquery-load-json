048e0cfa8c530116737fa0df827f3a6a0c2bd1f5
jocapc@gmail.com <jocapc@gmail.com@91f6fdaf-c773-323d-6236-93afe5a5d5da>
2011-10-22 22:51:45 +0000

    Version 1.2.2. updated changes from Sinergija presentation and added jquery mobile examples (this is still in progress)

---
+++ mobile/data18.js
@@ -0,0 +1,14 @@
+﻿{
+    "ID": 18,
+    "Name": "The Empire",
+    "Address": "Milton Keynes Leisure Plaza",
+    "Town": "London",
+    "Description": "Lorem ipsum...",
+    "Date": "\/Date(-62135596800000)\/",
+    "Contact": "Email",
+    "IsFeatured": false,
+    "Country": [
+        "UK" 
+    ],
+    "Logo":"/images/logo18.jpg$The Empire Logo"
+}
---
+++ mobile/list.js
@@ -0,0 +1,29 @@
+[
+    {
+        "ID": 17,
+        "Name": "Emkay Entertainments",
+        "Address": "Nobel House, Regent Centre",
+    	"Town": "Lothian",
+    	"Logo":"images/logo17.jpg"
+    },
+    {
+        "ID": 18,
+        "Name": "The Empire",
+        "Address": "Milton Keynes Leisure Plaza"
+    },
+    {
+        "ID": 19,
+        "Name": "Asadul Ltd",
+        "Address": "Hophouse"
+    },
+    {
+        "ID": 20,
+        "Name": "Star Records",
+        "Address": null
+    },
+    {
+        "ID": 21,
+        "Name": "Ashley Mark Publishing Company",
+        "Address": "1-2 Vance Court"
+    }
+]
