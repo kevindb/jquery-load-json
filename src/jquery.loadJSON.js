5b9cbfc71c81ae01e235d9d1b17f400745410afb
Kevin Morris <kmorri36@jhu.edu>
2017-03-03 15:27:52 -0500

    Registers plugin with AMD and CommonJS

--- src/jquery.loadJSON.js
+++ src/jquery.loadJSON.js
@@ -16,7 +16,32 @@
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
 */
 
-(function ($) {
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node/CommonJS
+        module.exports = function( root, jQuery ) {
+            if ( jQuery === undefined ) {
+                // require('jQuery') returns a factory that requires window to build a jQuery instance, we normalize how we use modules
+                // that require this pattern but the window provided is a noop if it's defined (how jquery works)
+                if ( typeof window !== 'undefined' ) {
+                    jQuery = require('jquery');
+                }
+                else {
+                    jQuery = require('jquery')(root);
+                }
+            }
+            factory(jQuery);
+            return jQuery;
+        };
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
+
+}(function ($) {
 	$.fn.loadJSON = function (obj, options) {
 		Array.prototype.contains = function(obj) {
 		    var i = this.length;
@@ -390,4 +415,4 @@
 			}
 		});
 	};
-})(jQuery);
+}));
